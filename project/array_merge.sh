51db20654a414fcbdf7ded9e6348dff67d67b5c6
= <=>
2018-01-31 01:32:44 +0500

    painless with java

--- project/array_merge.sh
+++ project/array_merge.sh
@@ -83,3 +83,29 @@ GET /posts/post/_search
       }
    }
 }
+#Autocomplete customization v2.2 (unique)
+GET /posts/post/_search
+{  
+   "_source":[  
+      "tag_keywords"
+   ],
+   "query":{  
+      "match":{  
+         "tag_keywords":{  
+            "query":"ali",
+            "analyzer":"standard"
+         }
+      }
+   },
+   "script_fields":{  
+      "tag_keywords_filtered":{  
+         "script":{  
+            "lang":"painless",
+            "source":"HashSet hs = new HashSet();List x = new ArrayList();for (int i = 0; i < params['_source']['tag_keywords'].length; ++i) { String s = params['_source']['tag_keywords'][i];if(s.startsWith(params.keyword)){ x.add(s);}} hs.addAll(x); x.clear();x.addAll(hs);return x;",
+            "params":{  
+               "keyword":"ali"
+            }
+         }
+      }
+   }
+}
