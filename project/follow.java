24bd7cb78cbb053fc49ff13c42d0ecc2cf457dfa
= <=>
2018-02-16 01:26:57 +0500

    final script

--- project/follow.java
+++ project/follow.java
@@ -2,14 +2,13 @@
 HashSet hs = new HashSet();
 if (ctx._source.id == params.follower_id){
 	List x = ctx._source.followings;
-	HashSet hs = new HashSet();
 	if(params.action == 1){
 		x.add(params.following_id);
 		hs.addAll(x);
 	}
 	else {
 		hs.addAll(x);
-		hs.values().remove(params.following_id);
+		hs.remove(params.following_id);
 	}
 	x.clear();
 	x.addAll(hs);
@@ -17,16 +16,20 @@ if (ctx._source.id == params.follower_id){
 }
 else if (ctx._source.id == params.following_id){
 	List x = ctx._source.followers;
-	HashSet hs = new HashSet();
 	if(params.action == 1){
 		x.add(params.follower_id);
 		hs.addAll(x);
 	}
 	else {
 		hs.addAll(x);
-		hs.values().remove(params.follower_id);
+		hs.remove(params.follower_id);
 	}
 	x.clear();
 	x.addAll(hs);
 	ctx._source.followers = x;
 }
+
+
+HashSet hs=new HashSet();if(ctx._source.id==params.follower_id){List x=ctx._source.followings;if(params.action==1){x.add(params.following_id);hs.addAll(x)}else{hs.addAll(x);hs.remove(params.following_id)}x.clear();x.addAll(hs);ctx._source.followings=x}else if(ctx._source.id==params.following_id){List x=ctx._source.followers;if(params.action==1){x.add(params.follower_id);hs.addAll(x)}else{hs.addAll(x);hs.remove(params.follower_id)}x.clear();x.addAll(hs);ctx._source.followers=x}
+
+HashSet hs=new HashSet();if(ctx._source.id==params.follower_id){List x=ctx._source.followings;if(params.action==1){x.add(params.following_id);hs.addAll(x)}else{hs.addAll(x);hs.remove(params.following_id)}x.clear();x.addAll(hs);ctx._source.followings=x}else if(ctx._source.id==params.following_id){List x=ctx._source.followers;if(params.action==1){x.add(params.follower_id);hs.addAll(x)}else{hs.addAll(x);hs.remove(params.follower_id)}x.clear();x.addAll(hs);ctx._source.followers=x}
