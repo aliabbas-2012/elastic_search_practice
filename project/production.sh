5bd1c89f03f0897865468c76ac082ea24bd5e678
Ali Abbas <ali.abbas@ilsainteractive.com>
2018-02-20 21:14:38 +0500

    production queries

--- project/production.sh
+++ project/production.sh
@@ -1,1 +1,57 @@
-curl -XGET  -H 'Content-Type: application/json' 'https://vpc-production1-new-node-js-d25dxb5wvxp5wwaih2qxwlqcve.us-west-2.es.amazonaws.com/posts/post/128094?pretty' 
+curl -XGET  -H 'Content-Type: application/json' 'https://vpc-production1-new-node-js-d25dxb5wvxp5wwaih2qxwlqcve.us-west-2.es.amazonaws.com/posts/post/128094?pretty'
+
+curl -XGET  -H 'Content-Type: application/json' 'https://vpc-production1-new-node-js-d25dxb5wvxp5wwaih2qxwlqcve.us-west-2.es.amazonaws.com/users/user/18368?pretty&_source=id,is_live,followings' 
+
+
+curl -XGET  -H 'Content-Type: application/json' 'https://vpc-production1-new-node-js-d25dxb5wvxp5wwaih2qxwlqcve.us-west-2.es.amazonaws.com/posts/post/_search?pretty' -d '
+{
+  "size":1000,
+  "_source": ["id","user_id","user_is_live"],
+   "query": { 
+      "bool":{
+        "must":{
+        "term": {
+          "user_id": 18368
+        }
+      } 
+    }
+  }
+}
+'
+curl -XGET  -H 'Content-Type: application/json' 'https://vpc-production1-new-node-js-d25dxb5wvxp5wwaih2qxwlqcve.us-west-2.es.amazonaws.com/posts/post/_search?pretty' -d '
+{
+  "size":1000,
+  "_source": ["id","user_id","user_is_live"],
+   "query": { 
+      "bool":{
+        "must":{
+        "term": {
+          "user_id": 28327
+        }
+      } 
+    }
+  }
+}
+'
+
+curl -XGET  -H 'Content-Type: application/json' 'https://vpc-production1-new-node-js-d25dxb5wvxp5wwaih2qxwlqcve.us-west-2.es.amazonaws.com/posts/_count?pretty' -d '
+{
+   "query": { 
+      "bool":{
+        "must":{
+        "term": {
+          "user_id": 28327
+        }
+      } 
+    }
+  }
+}
+'
+
+curl -XPUT  -H 'Content-Type: application/json' 'https://vpc-production1-new-node-js-d25dxb5wvxp5wwaih2qxwlqcve.us-west-2.es.amazonaws.com/posts/_settings/' -d '
+{ "index" : { "max_result_window" : 5000000 } }
+'
+
+curl -XPUT  -H 'Content-Type: application/json' 'https://vpc-production1-new-node-js-d25dxb5wvxp5wwaih2qxwlqcve.us-west-2.es.amazonaws.com/users/_settings/' -d '
+{ "index" : { "max_result_window" : 500000 } }
+'
