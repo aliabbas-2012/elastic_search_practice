326eab6063ea3ea1e9399848c815ba46550be828
= <itsgeniusstar@gmail.com>
2018-03-01 23:27:32 +0500

    follow and block changes

--- project/block.java
+++ project/block.java
@@ -1,36 +1,46 @@
 HashSet hs1 = new HashSet();
 HashSet hs2 = new HashSet();
+HashSet hs3 = new HashSet();
 if (ctx._source.id == params.user_id) {
     List x1 = ctx._source.followings;
     hs1.addAll(x1);
     hs1.remove(params.block_user_id);
 
-    x1.clear();
-    x1.addAll(hs1);
+    x1.clear();x1.addAll(hs1);
     ctx._source.followings = x1;
 
     List x2 = ctx._source.followers;
     hs2.addAll(x2);
     hs2.remove(params.block_user_id);
 
-    x2.clear();
-    x2.addAll(hs2);
-    ctx._source.followings = x2;
+    x2.clear();x2.addAll(hs2);
+    ctx._source.followers = x2;
+
+    List x3 = ctx._source.blocked_users;
+
+    x3.add(params.block_user_id);
+	hs3.addAll(x3);
+	x3.clear();x3.addAll(hs3);
+	ctx._source.blocked_users = x3;
 
 } else if (ctx._source.id == params.block_user_id) {
     List x1 = ctx._source.followings;
     hs1.addAll(x1);
     hs1.remove(params.user_id);
 
-    x1.clear();
-    x1.addAll(hs1);
+    x1.clear();x1.addAll(hs1);
     ctx._source.followings = x1;
 
     List x2 = ctx._source.followers;
-    hs2.addAll(x2);
-    hs2.remove(params.user_id);
+    hs2.addAll(x2);hs2.remove(params.user_id);
 
-    x2.clear();
-    x2.addAll(hs2);
-    ctx._source.followings = x2;
+    x2.clear();x2.addAll(hs2);
+    ctx._source.followers = x2;
+
+    List x3 = ctx._source.users_blocked;
+
+    x3.add(params.user_id);
+	hs3.addAll(x3);
+	x3.clear();x3.addAll(hs3);
+	ctx._source.users_blocked = x3;
 }
---
+++ project/block.sh
@@ -0,0 +1,22 @@
+#block user
+POST users/_update_by_query?refresh
+{
+	"conflicts": "proceed",
+	"script": {
+		"source": "HashSet hs1=new HashSet();HashSet hs2=new HashSet();HashSet hs3=new HashSet();if(ctx._source.id==params.user_id){List x1=ctx._source.followings;hs1.addAll(x1);hs1.remove(params.block_user_id);x1.clear();x1.addAll(hs1);ctx._source.followings=x1;List x2=ctx._source.followers;hs2.addAll(x2);hs2.remove(params.block_user_id);x2.clear();x2.addAll(hs2);ctx._source.followers=x2;List x3=ctx._source.blocked_users;x3.add(params.block_user_id);hs3.addAll(x3);x3.clear();x3.addAll(hs3);ctx._source.blocked_users=x3}else if(ctx._source.id==params.block_user_id){List x1=ctx._source.followings;hs1.addAll(x1);hs1.remove(params.user_id);x1.clear();x1.addAll(hs1);ctx._source.followings=x1;List x2=ctx._source.followers;hs2.addAll(x2);hs2.remove(params.user_id);x2.clear();x2.addAll(hs2);ctx._source.followers=x2;List x3=ctx._source.users_blocked;x3.add(params.user_id);hs3.addAll(x3);x3.clear();x3.addAll(hs3);ctx._source.users_blocked=x3}",
+		"lang": "painless",
+		"params": {
+			"user_id": 18368,
+			"block_user_id": 9595
+		}
+	},
+	"query": {
+		"terms": {
+			"id": [9595,18368]
+		}
+	}
+}    
+
+#Test
+GET /users/user/18368?_source=followings,followers
+GET /users/user/9595?_source=followings,followers
