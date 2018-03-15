b603801ba0bae94b51d80732a93cd154d883f265
Ali Abbas <ali.abbas@ilsainteractive.com>
2018-03-15 19:46:01 +0500

    v2.4

---
+++ project/production_v2.4.sh
@@ -0,0 +1,79 @@
+curl -XGET  -H 'Content-Type: application/json' 'https://vpc-production-node-v2-4-cgxa3u7bwmvrhu2mfjdlhzujzq.us-west-2.es.amazonaws.com/posts/post/128094?pretty'
+
+curl -XGET  -H 'Content-Type: application/json' 'https://vpc-production-node-v2-4-cgxa3u7bwmvrhu2mfjdlhzujzq.us-west-2.es.amazonaws.com/users/user/18368?pretty&_source=id,is_live,followings' 
+
+
+curl -XGET  -H 'Content-Type: application/json' 'https://vpc-production-node-v2-4-cgxa3u7bwmvrhu2mfjdlhzujzq.us-west-2.es.amazonaws.com/posts/post/_search?pretty' -d '
+{
+  "size":1000,
+  "_source": ["id","user_id","user_is_live"],
+   "query": { 
+      "bool":{
+        "must":{
+        "term": {
+          "user_id": 18368
+        }
+      } 
+    }
+  }
+}
+'
+curl -XGET  -H 'Content-Type: application/json' 'https://vpc-production-node-v2-4-cgxa3u7bwmvrhu2mfjdlhzujzq.us-west-2.es.amazonaws.com/posts/post/_search?pretty' -d '
+{
+  "size":1000,
+  "_source": ["id","user_id","user_is_live"],
+   "query": { 
+      "bool":{
+        "must":{
+        "term": {
+          "user_id": 2
+        }
+      } 
+    }
+  }
+}
+'
+
+curl -XGET  -H 'Content-Type: application/json' 'https://vpc-production-node-v2-4-cgxa3u7bwmvrhu2mfjdlhzujzq.us-west-2.es.amazonaws.com/users/user/_search?pretty' -d '
+{
+  "size":1000,
+  "_source": ["id","username","is_live"],
+   "query": { 
+      "bool":{
+        "must":{
+        "term": {
+          "id": 2
+        }
+      } 
+    }
+  }
+}
+'
+
+curl -XGET  -H 'Content-Type: application/json' 'https://vpc-production-node-v2-4-cgxa3u7bwmvrhu2mfjdlhzujzq.us-west-2.es.amazonaws.com/posts/_count?pretty' -d '
+{
+   "query": { 
+      "bool":{
+        "must":{
+        "term": {
+          "user_id": 28327
+        }
+      } 
+    }
+  }
+}
+'
+
+curl -XPUT  -H 'Content-Type: application/json' 'https://vpc-production-node-v2-4-cgxa3u7bwmvrhu2mfjdlhzujzq.us-west-2.es.amazonaws.com/posts/_settings/' -d '
+{ "index" : { "max_result_window" : 5000000 } }
+'
+
+curl -XPUT  -H 'Content-Type: application/json' 'https://vpc-production-node-v2-4-cgxa3u7bwmvrhu2mfjdlhzujzq.us-west-2.es.amazonaws.com/users/_settings/' -d '
+{ "index" : { "max_result_window" : 500000 } }
+'
+
+# post count
+
+curl -XGET  -H 'Content-Type: application/json' 'https://vpc-production-node-v2-4-cgxa3u7bwmvrhu2mfjdlhzujzq.us-west-2.es.amazonaws.com/posts/_count?pretty'
+
+curl -XGET  -H 'Content-Type: application/json' 'https://vpc-production-node-v2-4-cgxa3u7bwmvrhu2mfjdlhzujzq.us-west-2.es.amazonaws.com/users/_count?pretty'
