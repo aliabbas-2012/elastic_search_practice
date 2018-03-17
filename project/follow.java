3df3332bee2d6c191aa758a8d0a741abce5dafca
= <=>
2018-03-17 22:29:58 +0500

    apply sorting

--- project/follow.java
+++ project/follow.java
@@ -33,3 +33,22 @@ else if (ctx._source.id == params.following_id){
 HashSet hs=new HashSet();if(ctx._source.id==params.follower_id){List x=ctx._source.followings;if(params.action==1){x.add(params.following_id);hs.addAll(x)}else{hs.addAll(x);hs.remove(params.following_id)}x.clear();x.addAll(hs);ctx._source.followings=x}else if(ctx._source.id==params.following_id){List x=ctx._source.followers;if(params.action==1){x.add(params.follower_id);hs.addAll(x)}else{hs.addAll(x);hs.remove(params.follower_id)}x.clear();x.addAll(hs);ctx._source.followers=x}
 
 HashSet hs=new HashSet();if(ctx._source.id==params.follower_id){List x=ctx._source.followings;if(params.action==1){x.add(params.following_id);hs.addAll(x)}else{hs.addAll(x);hs.remove(params.following_id)}x.clear();x.addAll(hs);ctx._source.followings=x}else if(ctx._source.id==params.following_id){List x=ctx._source.followers;if(params.action==1){x.add(params.follower_id);hs.addAll(x)}else{hs.addAll(x);hs.remove(params.follower_id)}x.clear();x.addAll(hs);ctx._source.followers=x}
+
+
+// add following_id in multiple users
+List x= ctx._source.followings;
+HashSet hs = new HashSet();
+if(x!=null){
+	x = new ArrayList();
+}
+x.clear();
+hs.addAll(x);
+hs.add(params.following_id);
+x.addAll(hs);
+return x;
+
+//above code compresss
+List x = ctx._source.followings;HashSet hs=new HashSet();if(x==null){x=new ArrayList();} hs.addAll(x);x.clear();hs.add(params.following_id);x.addAll(hs);ctx._source.followings = x.sort();
+
+//above vise versa
+List x = ctx._source.followers;HashSet hs=new HashSet();if(x==null){x=new ArrayList();} hs.addAll(x);x.clear();hs.add(params.follower_id);x.addAll(hs);ctx._source.followers = x.sort();
