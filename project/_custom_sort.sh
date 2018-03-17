e30751159396d5697b9d296cf5ac3da8fe3c6ac3
= <=>
2018-03-17 23:06:44 +0500

    custom sort

--- project/_custom_sort.sh
+++ project/_custom_sort.sh
@@ -53,3 +53,65 @@ GET /users/user/_search
         }
     }
 }
+
+# GET those whome followings alot
+GET /users/user/_search
+{
+	"_source": [
+		"id",
+		"username",
+		"full_name"
+	],
+	"script_fields":{
+		"total_followings": {
+            "script": {
+                "lang": "painless",
+                "source": "return params._source.followings.length"
+            }
+        }
+	},
+	"sort" : {
+        "_script" : {
+            "type": "number",
+            "script" : {
+                "lang": "painless",
+                "source": "return params._source.followings.length ",
+                "params" : {
+                    "factor" : 1.1
+                }
+            },
+            "order" : "desc"
+        }
+    }
+}
+
+# GET those whome followers alot
+GET /users/user/_search
+{
+	"_source": [
+		"id",
+		"username",
+		"full_name"
+	],
+	"script_fields":{
+		"total_followers": {
+            "script": {
+                "lang": "painless",
+                "source": "return params._source.followers.length"
+            }
+        }
+	},
+	"sort" : {
+        "_script" : {
+            "type": "number",
+            "script" : {
+                "lang": "painless",
+                "source": "return params._source.followers.length ",
+                "params" : {
+                    "factor" : 1.1
+                }
+            },
+            "order" : "desc"
+        }
+    }
+}
