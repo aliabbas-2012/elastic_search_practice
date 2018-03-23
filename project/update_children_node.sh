c590927ad98fd7b6dff9605e0682f7e98114d641
= <=>
2018-03-23 22:24:48 +0500

    update box will also update tag keywords

--- project/update_children_node.sh
+++ project/update_children_node.sh
@@ -52,7 +52,7 @@ POST posts/post/_update_by_query?refresh
 {
     "conflicts": "proceed",
     "script": {
-        "source": "List x=ctx._source.boxes;for(int i=0;i<x.size();i++){if(x.get(i).id.equals(params.box_id)){x.get(i).name=params.new_name;}}ctx._source.boxes=x;",
+        "source": "List x=ctx._source.boxes;String old_name;for(int i=0;i<x.size();i++){if(x.get(i).id.equals(params.box_id)){old_name=x.get(i).name;x.get(i).name=params.new_name;}}ctx._source.boxes=x;HashSet hs=new HashSet();List xt=ctx._source.tag_keywords;xt.removeIf(Objects::isNull);xt.replaceAll(String::toLowerCase);hs.addAll(xt);hs.remove(old_name.toLowerCase());hs.add(params.new_name.toLowerCase());xt.clear();xt.addAll(hs);ctx._source.tag_keywords=xt;",
         "lang": "painless",
         "params": {
             "user_id":18368,
