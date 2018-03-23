b73c7f710464bc9a70cfd88511c3d3c84e5e7af7
= <=>
2018-03-23 22:16:44 +0500

    update box name

--- project/update_children_node.java
+++ project/update_children_node.java
@@ -12,11 +12,24 @@ List x=params._source.boxes;for(int i=0;i<x.size();i++){if(x.get(i).id.equals(pa
 //Update by query 
 
 List x = ctx._source.boxes;
+String old_name = "";
 for (int i = 0; i < x.size(); i++) {
-	if(x.get(i).id.equals(params.box_id)){
-		x.get(i).name = params.new_name;
-	}
-} ctx._source.boxes = x;
+ if (x.get(i).id.equals(params.box_id)) {
+ 	old_name = x.get(i).name;
+  	x.get(i).name = params.new_name;
+ }
+} 
+ctx._source.boxes = x;
+HashSet hs = new HashSet();
+List xt = ctx._source.tag_keywords;
+xt.removeIf(Objects::isNull);
+xt.replaceAll(String::toLowerCase);
+hs.addAll(xt);
+hs.remove(old_name.toLowerCase());
+hs.add(params.new_name.toLowerCase());
+xt.clear();xt.addAll(hs);
+ctx._source.tag_keywords=x;
 
 //minfy
-List x=ctx._source.boxes;for(int i=0;i<x.size();i++){if(x.get(i).id.equals(params.box_id)){x.get(i).name=params.new_name;}}ctx._source.boxes=x;
+List x=ctx._source.boxes;String old_name="";for(int i=0;i<x.size();i++){if(x.get(i).id.equals(params.box_id)){old_name=x.get(i).name;x.get(i).name=params.new_name;}}
+ctx._source.boxes=x;HashSet hs=new HashSet();List xt=ctx._source.tag_keywords;xt.removeIf(Objects::isNull);xt.replaceAll(String::toLowerCase);hs.addAll(xt);hs.remove(old_name.toLowerCase());hs.add(params.new_name.toLowerCase());xt.clear();xt.addAll(hs);ctx._source.tag_keywords=x;
