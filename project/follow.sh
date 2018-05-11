4dccb247267f0ba871c2ce1ad71c452f753d18eb
Ali Abbas <ali.abbas@ilsainteractive.com>
2018-05-11 16:35:53 +0500

    elastic search

--- project/follow.sh
+++ project/follow.sh
@@ -66,3 +66,102 @@ GET /trending/doc/_search
     }
   }
 }
+
+
+
+GET /trending/doc/_search
+{
+  "query": {
+    "bool": {
+      "must": [
+        {
+          "term": {
+            "type": "user"
+          }
+        },
+        {
+          "term": {
+            "id": "u-14"
+          }
+        },
+        {
+          "bool": {
+            "must_not": [
+              {
+                "has_child": {
+                  "type": "followers",
+                  "inner_hits": {
+                    "_source": [
+                      "user_id"
+                    ]
+                  },
+                  "query": {
+                    "bool": {
+                      "must": [
+                        {
+                          "term": {
+                            "user_id": "u-17118"
+                          }
+                        }
+                      ]
+                    }
+                  }
+                }
+              }
+            ]
+          }
+        }
+      ]
+    }
+  }
+}
+GET /trending/doc/_search
+{
+  "_source": [
+    "user_id",
+    "object_id"
+  ],
+  "size": 2000,
+  "query": {
+    "bool": {
+      "must": [
+        {
+          "term": {
+            "type": "followings"
+          }
+        },
+        {
+          "term": {
+            "object_id": "u-30"
+          }
+        },
+        {
+          "has_parent": {
+            "parent_type": "user",
+            "inner_hits": {
+              "_source": [
+                "username"
+              ]
+            },
+            "query": {
+              "bool": {
+                "must": [
+                  {
+                    "multi_match": {
+                        "analyzer": "standard",
+                        "query": "fahad",
+                        "type": "phrase_prefix",
+                        "fields": ["username", "full_name"],
+                        "operator": "or",
+                        "minimum_should_match": "100%"
+                    }
+                  }
+                ]
+              }
+            }
+          }
+        }
+      ]
+    }
+  }
+}
