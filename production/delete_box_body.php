791ed4d829dacdd25fafba5890c16738ed937489
Ali Abbas <ali.abbas@ilsainteractive.com>
2018-05-30 17:40:49 +0500

    version

--- production/delete_box_body.php
+++ production/delete_box_body.php
@@ -29,3 +29,12 @@
         ]
     ]
 ]
+
+
+57649
+
+
+183022
+
+sudo a2dismod php7.0 ; sudo a2enmod php5 ; sudo service apache2 restart
+sudo update-alternatives --set php /usr/bin/php5
--- project/delete_follow_from_block.sh
+++ project/delete_follow_from_block.sh
@@ -38,7 +38,7 @@ curl -XPOST -H 'Content-Type: application/json' 'https://search-staging-node-v2-
   }
 }'
 
-curl -XPOST -H 'Content-Type: application/json' 'http://localhost:9200/trending/doc/_delete_by_query' -d '
+curl -XPOST -H 'Content-Type: application/json' 'https://vpc-production1-new-node-js-d25dxb5wvxp5wwaih2qxwlqcve.us-west-2.es.amazonaws.com/trending/doc/_delete_by_query' -d '
 {
   "query": {
     "bool": {
@@ -57,7 +57,25 @@ curl -XPOST -H 'Content-Type: application/json' 'http://localhost:9200/trending/
     }
   }
 }'
-
+curl -XGET -H 'Content-Type: application/json' 'https://vpc-production1-new-node-js-d25dxb5wvxp5wwaih2qxwlqcve.us-west-2.es.amazonaws.com/trending/doc/_count?pretty' -d '
+{
+  "query": {
+    "bool": {
+      "should": [
+        {
+          "term": {
+            "type": "followings"
+          }
+        },
+        {
+          "term": {
+            "type": "followers"
+          }
+        }
+      ]
+    }
+  }
+}'
 curl -XGET -H 'Content-Type: application/json' https://search-staging-node-v2-4-5zns7u6jcfgr4ohsvds54etdhu.us-west-2.es.amazonaws.com/trending/doc/_count?pretty -d '
 {
   "query" : {
