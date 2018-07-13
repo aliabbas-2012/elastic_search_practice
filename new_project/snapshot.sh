8a63782db62a604594d904bb9962d88587b3bd95
Ali Abbas <ali.abbas@ilsainteractive.com>
2018-07-13 19:56:44 +0500

    elastic search

---
+++ new_project/snapshot.sh
@@ -0,0 +1,16 @@
+curl -XPUT -H 'Content-Type: application/json'  'http://127.0.0.1:9200/_snapshot/my_backup' -d '
+{
+  "type": "fs",
+  "settings": {
+    "location": "my_backup_location"
+  }
+}
+'
+
+curl -XPUT -H 'Content-Type: application/json'  'http://127.0.0.1:9200/_snapshot/my_backup/snapshot_2?wait_for_completion=true' -d ' 
+{
+  "indices": "trending",
+  "ignore_unavailable": true,
+  "include_global_state": false
+}
+'
