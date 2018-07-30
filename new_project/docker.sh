7a71f4a9e37d329cdf6e6597d8e788f33e07ece6
= <=>
2018-07-31 01:24:28 +0500

    test

---
+++ new_project/docker.sh
@@ -0,0 +1,11 @@
+sudo docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.3.2
+
+
+#run using volume
+sudo docker run --name devtest1 -p 9200:9200 -p 9300:9300 -v foo:/app -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.3.2
+
+
+docker volume create --driver local \
+    --opt type=btrfs \
+    --opt device=/home/ali/test-docker \
+    foo
