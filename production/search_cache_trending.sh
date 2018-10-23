27bfda8d0e8afc0817326ddf4ec6f9879c48158b
Ali Abbas <ali.abbas@ilsainteractive.com>
2018-10-23 21:34:31 +0500

    test

---
+++ production/search_cache_trending.sh
@@ -0,0 +1,45 @@
+curl -XPUT "http://localhost:9200/people-search" -H 'Content-Type: application/json' -d'
+{
+"settings": {
+  "index": {
+    "max_result_window": 5000000
+  }
+},
+"mappings": {
+  "doc": {
+    "properties": {
+      "id": {
+        "type": "text",
+        "index": true
+      },
+
+      "user_id": {
+        "type": "integer",
+        "index": true
+      },
+      "type": {
+        "type": "keyword",
+        "index": true
+      },
+      "created_at": {
+        "type": "date",
+        "fields": {
+          "raw": {
+            "type": "keyword",
+            "index": false
+          }
+        }
+      },
+      "indexed_at": {
+        "type": "date",
+        "fields": {
+          "raw": {
+            "type": "keyword",
+            "index": false
+          }
+        }
+      }
+    }
+  }
+}
+}'
